03a196182ec5b60ed91d477fb5d4a878ee6dc73f
Mohan Timilsina <timilsinamohan@gmail.com>
2012-08-16 10:53:27 +0700

    This is the first commit for the application

---
+++ ContactListActivity1/src/com/example/contactlistactivity1/ContactAdapter1.java
@@ -0,0 +1,70 @@
+package com.example.contactlistactivity1;
+
+
+import java.util.List;
+
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ContactAdapter1 extends ArrayAdapter<Contact1> {
+
+	private final List<Contact1> _contacts;
+	private final Activity _context;
+	
+	public ContactAdapter1(Activity context, List<Contact1> contacts)
+	{
+		super(context,R.layout.contactlistitem,contacts);
+		this._contacts=contacts;
+		this._context=context;
+	}
+	static class ViewHolder {
+		protected TextView text;
+		private Contact1  _contact;
+		public ImageView imageview;
+		protected void setContact(Contact1 contact)
+		{
+			text.setText(contact.getDisplayName());
+			imageview.setImageBitmap(contact.getImage());
+			_contact=contact;
+		}
+		protected Contact1 getContact() {return _contact;}
+	}
+	@Override
+	public Contact1 getItem(int position)
+	{
+		return _contacts.get(position);
+	}
+	@Override
+	public View getView(final int position, View convertView, ViewGroup parent)
+	{
+		
+		View view=null;
+		
+
+		if(convertView==null)
+		{
+			LayoutInflater inflater=_context.getLayoutInflater();
+			view=inflater.inflate(R.layout.contactlistitem, null);
+			final ViewHolder viewHolder=new ViewHolder();
+			viewHolder.text=(TextView)view.findViewById(R.id.txtDisplayName);
+			viewHolder.imageview =(ImageView)view.findViewById(R.id.contact_image);
+			viewHolder.setContact(_contacts.get(position));
+			view.setTag(viewHolder);
+			
+		}
+		
+		else 
+		{
+			view = convertView;
+		}
+		
+		return view;
+	}
+}
+
---
+++ ContactListActivity1/src/com/example/contactlistactivity1/ContactListActivity1.java
@@ -0,0 +1,109 @@
+package com.example.contactlistactivity1;
+
+import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.app.ListActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.provider.ContactsContract.Data;
+
+public class ContactListActivity1 extends ListActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContactList1 contactList=this.getContacts();
+        ArrayAdapter<Contact1> adapter=new ContactAdapter1(this,contactList.getContacts());
+        setListAdapter(adapter);
+        
+        
+    }
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id)
+    {
+    	super.onListItemClick(l, v, position, id);
+    	Object o=this.getListAdapter().getItem(position);
+    	Contact1 c=(Contact1)o;
+    	Toast.makeText(this, c.getDisplayName(), Toast.LENGTH_SHORT).show();
+    	Toast.makeText(this, c.getId(), Toast.LENGTH_SHORT).show();
+    }
+    
+    private ContactList1 getContacts()
+    {
+    	ContactList1 contactList=new ContactList1();
+    	Uri uri=ContactsContract.Contacts.CONTENT_URI;
+    	ContentResolver cr=getContentResolver();
+    	String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+    	Cursor cur=cr.query(uri, null, null, null, sortOrder);
+    	
+    	if(cur.getCount() >0)
+    	{
+    		String id;
+    		String img;
+    		String name;
+    		while(cur.moveToNext())
+    		{
+    			Contact1 c =new Contact1();
+    			id = cur.getString(cur.getColumnIndex(ContactsContract.Contacts._ID));
+    		    img= cur.getString(cur.getColumnIndex(ContactsContract.Contacts.PHOTO_ID));
+    			name=cur.getString(cur.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+    			
+    			final Bitmap photo;
+    			if(img != null) {
+    				photo = queryContactBitmap(img);
+    				
+    				
+    			} else {
+    				photo = null;
+    			}
+    			
+    			
+    			    			
+    			c.setId(id);
+    			//c.setId(img);
+    		
+    			c.setImage(photo);
+    			c.setDisplayName(name);
+    			contactList.addContact(c);
+    			
+    			
+    		}
+    	}
+    //	cur.close();
+    	return contactList;
+    }
+    private Bitmap queryContactBitmap(String photoId) {
+		final Cursor photo = managedQuery(
+				Data.CONTENT_URI,
+				new String[] {Photo.PHOTO},		// column where the blob is stored
+				Data._ID + "=?",				// select row by id
+				new String[]{photoId},			// filter by the given photoId
+				null);
+		
+		
+		final Bitmap photoBitmap;
+		if(photo.moveToFirst()) {
+			byte[] photoBlob = photo.getBlob(
+					photo.getColumnIndex(Photo.PHOTO));
+			photoBitmap = BitmapFactory.decodeByteArray(
+					photoBlob, 0, photoBlob.length);
+		} else {
+			photoBitmap = null;
+		}
+		photo.close();
+		
+		return photoBitmap;
+		
+		
+	}
+}
